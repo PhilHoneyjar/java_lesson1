422f0aa41cf8e9b0ec4bb79e5e78247e0a6b7488
Philipp Medoev <phil.medoev@gmail.com>
2023-03-05 18:09:44 +0300

    Домашка

--- t1.java
+++ t1.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class t1 {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter number: ");
-        int a= sc.nextInt();
+        int a = sc.nextInt();
 
         System.out.println("Triangle is: " + getTriangle(a));
         System.out.println("Factorial is: " + getFactorial(a));
